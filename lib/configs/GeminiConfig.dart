174281b248843701777da05b5e779a12356e25ba
Hariprasathk13 <hariprasathh15@gmail.com>
2025-12-06 12:26:30 +0530

    env config

--- lib/configs/GeminiConfig.dart
+++ lib/configs/GeminiConfig.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 abstract class GeminiConfig {
   static final String baseUrl = dotenv.env['GEMINI_BASEURL']!;
   static final options = {
-    'x-goog-api-key': dotenv.env['x-goog-api-key']!,
+    'x-goog-api-key': dotenv.env['x_goog_api_key']!,
     'Content-Type': 'application/json'
   };
   static final List<Map<String, dynamic>> body = [
--- lib/configs/TranscriptorConfig.dart
+++ lib/configs/TranscriptorConfig.dart
@@ -3,8 +3,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 abstract class TranscriptorConfig {
   static String baseurl = dotenv.env['TRANSCRIPT_BASEURL']!;
   static Map<String, String> options = {
-    'x-rapidapi-key': dotenv.env['x-rapidapi-key']!,
-    'x-rapidapi-host': dotenv.env['x-rapidapi-host']!
+    'x-rapidapi-key': dotenv.env['x_rapidapi_key']!,
+    'x-rapidapi-host': dotenv.env['x_rapidapi_host']!
   };
   static String tailors = "?format_subtitle=srt&format_answer=json";
   static String getUrl(String id) {
